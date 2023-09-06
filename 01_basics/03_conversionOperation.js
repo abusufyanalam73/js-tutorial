788f2bd51900899037985f6222c0c006b2d772e4
abusufyanalam73 <abusufyanalam00786@gmail.com>
2023-09-06 14:28:34 +0530

    Operation.

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,3 +23,62 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber);
+
+// *****************Operation***********************
+
+let value = 3
+let negValue  = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Abusufyan"
+
+let str3 = str1 + str2 
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log((1 + 2) + "2");
+
+console.log((1 + 2) * 2 / 5 % 6 - 7);
+
+// Conversion =>
+ console.log(+true);
+
+//  console.log(true+) // error
+
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100
+++ gameCounter;
+console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+//If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+
+//If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
