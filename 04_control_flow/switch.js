c05133b281c85dc0a442838df69c2ef05ab466f0
abusufyanalam73 <abusufyanalam00786@gmail.com>
2023-09-16 15:39:08 +0530

    Control_flow-Switch-Truthy-Falsy-value

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,38 @@
+
+// switch case basic syntax
+// switch (key) {
+//     case value:
+
+//          break;
+
+
+//     default:
+//         break:     
+
+// }
+
+const month = 4
+
+switch (month) {
+    case "jan":
+        console.log("january")
+         break;
+    case "Feb":
+        console.log("Feb")
+         break;
+    case 3:
+        console.log("Mar")
+         break;
+    case 4:
+        console.log("April")
+         break;
+    case 5:
+        console.log("May")
+         break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+
